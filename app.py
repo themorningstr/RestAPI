779755e52c367ddae9bd13ae53b821b78a679517
vishwash Thakur <vishwasthakur35@gmail.com>
2023-12-19 02:18:29 +0530

    Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 stores  = [
     {
         "name" : "My Store",
-        "item" : [
+        "items" : [
             {
                 "name": "chair", 
                 "price": 12.00
@@ -24,3 +24,36 @@ stores  = [
 @app.get("/store")
 def get_store():
     return {"store" : stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items":[]}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"], "message": "you rock"}
+    return {"message": "store not found"}, 404
